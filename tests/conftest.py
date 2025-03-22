661c2982693e5d5c7980f2804e0b97ced417f34b
Daniel Tom <d.e.tom89@gmail.com>
2025-03-22 17:43:40 +0100

    Fix venv path

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,13 @@ import pytest
 from pyspark.sql import SparkSession
 import os
 
-os.environ["PYSPARK_PYTHON"] = "../.venv/bin/python"
 
+# Determine the current working directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+venv_python_path = os.path.join(parent_dir, ".venv/bin/python")
+
+os.environ["PYSPARK_PYTHON"] = venv_python_path
 
 @pytest.fixture(scope="module")
 def spark():
